14ace6afe6dfe7fade0302bd22df346ee8a82f4e
ZhaoDongyu <zhaodongyu@pku.edu.cn>
2022-08-23 22:38:33 +0800

    add project5

---
+++ PowerTransformation/PowerTransformation/main.cpp
@@ -0,0 +1,125 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <math.h>
+//#include <string.h>  
+
+#define height  600  
+#define width   455 
+
+typedef unsigned char  BYTE;    // 定义BYTE类型，占1个字节 
+//s = cr v
+int c = 1;
+float v = 0.6;
+float temp;
+int main(void)
+{
+	FILE *fp = NULL;
+	BYTE *ptr;
+
+	BYTE **Pic = new BYTE *[height];
+	for (int i = 0; i != height; ++i)
+	{
+		Pic[i] = new BYTE[width];
+	}
+
+	int i, j;
+
+	fp = fopen("untitled.raw", "rb");
+	ptr = (BYTE*)malloc(width * height * sizeof(BYTE));//创建内存
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			fread(ptr, 1, 1, fp);
+			Pic[i][j] = *ptr;  // 把图像输入到2维数组中,变成矩阵型式  
+			ptr++;
+		}
+	}
+	fclose(fp);
+
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			temp = c * pow( Pic[i][j]/255.0 , 10 )*255; 
+			//temp = c * pow(Pic[i][j], 0.6);
+			Pic[i][j] = (char)temp;
+		}
+	}
+
+	fp = fopen("10.raw", "wb");
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			fwrite(&Pic[i][j], 1, 1, fp);
+		}
+	}
+	fclose(fp);
+	return 0;
+}
+
+/*
+int main(void)
+{
+FILE *fp = NULL;
+
+BYTE B[height][width];
+BYTE *ptr;
+
+char path[256];
+char outpath[256];
+
+int i, j;
+
+// 输入源路径并打开raw图像文件
+printf("Input the raw image path: ");
+scanf("%s", path);
+if ((fp = fopen(path, "rb")) == NULL)
+{
+printf("can not open the raw image ");
+return 1;
+}
+else
+{
+printf("read OK");
+}
+
+// 分配内存并将图像读到二维数组中
+ptr = (BYTE*)malloc(width * height * sizeof(BYTE));
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+fread(ptr, 1, 1, fp);
+B[i][j] = *ptr;  // 把图像输入到2维数组中,变成矩阵型式
+printf("%d  ", B[i][j]);
+ptr++;
+}
+}
+fclose(fp);
+
+// 这里可以对二维数组中的图像数据进行处理
+
+// 将处理后的图像数据输出至文件
+printf("Input the raw_image path for save: ");
+scanf("%s", outpath);
+if ((fp = fopen(outpath, "wb")) == NULL)
+{
+printf("can not create the raw_image : %s\n", outpath);
+system("pause");
+return 1;
+}
+
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+fwrite(&B[i][j], 1, 1, fp);
+}
+}
+fclose(fp);
+return 0;
+}
+*/
